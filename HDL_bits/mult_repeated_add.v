857937db973408a8df07ee10d0bcc0d839b266bb
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-01-07 17:01:39 -0700

    Added CNTR to mult_repeated_add.v

--- HDL_bits/mult_repeated_add.v
+++ HDL_bits/mult_repeated_add.v
@@ -60,3 +60,44 @@ module EQZ(
     assign eqz = (data == 0);
     
 endmodule
+
+module CNTR (
+    dout,din,ld,dec,clk
+);
+  input [15:0] din;
+  input ld,clk,dec;
+  output reg [15:0] dout;
+  always @(posedge clk) begin
+    if (ld) dout<=din;
+    else if (dec) begin
+        dout <= dout - 1;
+    end
+  end  
+endmodule
+
+module controller (
+    LdA,LdB,LdP,clrP,decB,done,clk,eqz,start
+);
+    input clk,eqz,start;
+    output reg LdA,LdB,LdP,clrP,decB,done;
+    reg [2:0] state;
+    parameter 
+    s0 = 3'b000,
+    s1 = 3'b001,
+    s2 = 3'b010,
+    s3 = 3'b011,
+    s4 = 3'b100;
+
+    always @(posedge clk) begin
+        case (state)
+            s0: if(start) state<=s1;
+            s1: state<=s2;
+            s2: state<=s3;
+            s3: #2 if(eqz) state <= s4;
+            s4: state<=s4;
+            default: state<=s0;
+        endcase
+    end
+
+
+endmodule

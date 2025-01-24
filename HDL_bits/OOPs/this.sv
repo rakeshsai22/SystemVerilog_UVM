b58a3c176a0719043f05460e97d9c67e5f20f898
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-01-23 23:42:30 -0700

    updated HDL_bits/OOPs/this.sv

--- HDL_bits/OOPs/this.sv
+++ HDL_bits/OOPs/this.sv
@@ -3,3 +3,12 @@
 // can only be used within non-static methods,constraints and covergroups.
 
 // "this" is a built-in pre-defined object handle that refers to the current instance/object of a class. It is used
+
+class pjt;
+    bit [31:0] addr;
+
+    function new (bit [31:0] _addr);
+        this.addr = _addr; // addr variable in pkt class should be assigned with local variable addr in new()
+    endfunction
+    
+endclass

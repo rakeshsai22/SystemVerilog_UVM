e22f615ec695dbc60f54cd13885263912335d566
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-02-11 23:20:35 -0700

    updated HDL_bits/UVM/factory/example.sv

--- HDL_bits/UVM/factory/example.sv
+++ HDL_bits/UVM/factory/example.sv
@@ -1,9 +1,32 @@
+//  UVM Factory: a large LUT where all the uvm components and objects are registered and created.
+//  we use these macros to register our objs and comps in the factory
+//  `uvm_component_utils -> to register the components (uvm_driver, uvm_monitor, uvm_agent, uvm_env, uvm_test)
+//  `uvm_object_utils -> to register the object (uvm_sequence, uvm_sequence_item, uvm_transaction)
+
 // example of uvm factory
 // 1. create a class
 // 2. register the class with uvm factory
 // 3. create an instance of the class using uvm factory
 // 4. use the instance
 // 5. unregister the class with uvm factory
+
+// SV example
+class uart_tb;
+    uart_generator generator;
+    uart_driver driver;
+
+    function new();
+        generator = new();
+        driver = new();
+    endfunction
+    task run();
+        logic[7:0] data;
+        data = 8'hFF;
+        generator.write(data);
+        driver.read(data);
+    endtask
+
+endclass
 
 module example;
     class my_class;

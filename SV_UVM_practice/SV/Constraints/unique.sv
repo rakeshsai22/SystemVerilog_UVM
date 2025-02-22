dd8e2171d640948e7829df16f53c28be2c60919f
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-02-21 22:15:32 -0700

    updated SV_UVM_practice/SV/Constraints/unique.sv

--- SV_UVM_practice/SV/Constraints/unique.sv
+++ SV_UVM_practice/SV/Constraints/unique.sv
@@ -7,6 +7,12 @@ typedef enum  { r,a,n,d,o,m } scale_e;
 class seq_item;
     rand bit [3:0] arr_s[5];
     rand bit [3:0] arr_d[];
-    rand
+    rand bit [7:0] arr_a[scale_e];
+
+    rand bit [2:0] val1,val2,val3,val4;
+    scale_e scale;
+
+    constraint c_arr_s {unique {arr_s};}
+    constraint c_arr_d {unique {arr_d};}
     
 endclass

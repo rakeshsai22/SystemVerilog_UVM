fede5d1b0860c7a06c2a9b7bb5d21c755d357e80
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-02-21 23:42:13 -0700

    updated SV_UVM_practice/SV/Constraints/unique.sv SV_UVM_practice/SV/Verilog/asc_arr.sv

--- SV_UVM_practice/SV/Constraints/unique.sv
+++ SV_UVM_practice/SV/Constraints/unique.sv
@@ -5,9 +5,9 @@
 typedef enum  { r,a,n,d,o,m } scale_e;
 
 class seq_item;
-    rand bit [3:0] arr_s[5];
-    rand bit [3:0] arr_d[];
-    rand bit [7:0] arr_a[scale_e];
+    rand bit [3:0] arr_s[5]; //static arr
+    rand bit [3:0] arr_d[]; //dynamic arr
+    rand bit [7:0] arr_a[scale_e]; //associative arr
 
     rand bit [2:0] val1,val2,val3,val4;
     scale_e scale;
---
+++ SV_UVM_practice/SV/Verilog/asc_arr.sv
@@ -0,0 +1,9 @@
+// Associative array stores entries in a sparse matrix format. 
+// these arrs allocate the storage only when it is used, unless like in the dynamic array where the storage is allocated at the time of declaration.
+// the index expression is not restricted to a simple integer or string, it can be any data type.
+// Associative arrays are not allowed in the class, they can be declared only in the module or program block.
+// Associative array implements a lookup table of the elements of its declared type. The data type to be used as an index serves as lookup key and imposes an ordering
+
+//  Usage : When the size of the collection is unknown or the data space is sparse.
+
+// data_type array_name [index_type] ;

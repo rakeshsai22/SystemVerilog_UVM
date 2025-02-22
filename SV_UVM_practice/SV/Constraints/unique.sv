02529d0bced68dec342b55e83b396511ed453907
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-02-22 02:38:35 -0700

    updated SV_UVM_practice/SV/Constraints/unique.sv SV_UVM_practice/SV/Verilog/asc_arr.sv

--- SV_UVM_practice/SV/Constraints/unique.sv
+++ SV_UVM_practice/SV/Constraints/unique.sv
@@ -12,7 +12,34 @@ class seq_item;
     rand bit [2:0] val1,val2,val3,val4;
     scale_e scale;
 
-    constraint c_arr_s {unique {arr_s};}
-    constraint c_arr_d {unique {arr_d};}
+    constraint c_arr_s {unique {arr_s};} // unique constraint for static array
+    constraint c_arr_d { unique {arr_d}; 
+        arr_d.size() ==5;}  
+    constraint value_a_c { unique {arr_a};
+        arr_a.size == scale.num();}
+    constraint val_c {unique {val1,val2,val3,val4};}
     
 endclass
+
+module cons_unique;
+    seq_item item;
+    initial begin
+        item =new();
+
+        repeat(3) begin
+            item.randomize();
+            $display("val1 =%0d, val2 = %0d, val3 = %0d, val4 = %0d",item.val1,item.val2,item.val3,item.val4);
+
+            foreach (item.arr_s[i]) begin
+                $display("arr_s[%0d] = %0d",i,item.arr_s[i]);
+            end
+            foreach (item.arr_d[i]) begin
+                $display("arr_d[%0d] = %0d",i,item.arr_d[i]);
+            end
+            foreach (item.arr_a[i]) begin
+                $display("arr_a[%0d] = %0d",i,item.arr_a[i]);
+            end
+        end
+    end
+    
+endmodule
--- SV_UVM_practice/SV/Verilog/asc_arr.sv
+++ SV_UVM_practice/SV/Verilog/asc_arr.sv
@@ -7,3 +7,48 @@
 //  Usage : When the size of the collection is unknown or the data space is sparse.
 
 // data_type array_name [index_type] ;
+
+int a_arr[*]; // assoc_arr of int type with unspecified index
+bit [31:0] b_arr[string]; // assoc_arr of 32-bit type with string index
+ev_arr [myclass]; //assoc_arr of event type with myclass index
+
+// num() method returns the number of elements in the associative array
+// delete() method removes the element from the associative array
+// exists() method checks if the element exists in the associative array
+// first(var) method assigns teh value of first index to the variable var
+// last(var) method assigns teh value of last index to the variable var
+// next(index,var) method assigns the value of the next index to the variable var
+// prev(index,var) method assigns the value of the previous index to the variable var
+// rand() method returns a random index from the associative array
+// size() method returns the size of the associative array
+// unique() method returns 1 if the associative array has unique elements, else returns 0
+// values() method returns the values of the associative array
+
+module assoc_arr;
+    int a_arr[*];
+    int index;
+    int i =0;
+
+    initial begin
+        // allocating arr and assigning vals to it
+        repeat(5) begin
+            a_arr[index] = index*2;
+            index=index+4;
+        end
+        $display("\t Num of elements in the associative array : %0d",a_arr.num());
+        $display("## Assoc arr entries and values ###");
+        foreach (a_arr[i]) begin
+            $display("a_arr[%0d]= %0d",i,a_arr[i]);
+        end
+
+        // first and last index
+        $display("first element in assoc arr : %0d",a_arr.first())
+        $display("last element in assoc arr : %0d",a_arr.last())
+
+        a_arr.first(index);
+        $display("\First element in a_arr \t a_arr[%0d] = %0d ", index,a_arr[index]);
+
+        
+
+    end
+endmodule

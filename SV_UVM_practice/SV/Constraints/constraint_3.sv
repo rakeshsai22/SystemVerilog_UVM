e1b690a327d015a2b270f1ce4c8639b7e0bd180d
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-03-21 09:30:50 -0700

    updated SV_UVM_practice/SV/Constraints/constraint_3.sv

--- SV_UVM_practice/SV/Constraints/constraint_3.sv
+++ SV_UVM_practice/SV/Constraints/constraint_3.sv
@@ -32,9 +32,46 @@ module top;
     end
     endmodule
 
+// output:
+// size of arr = 15
+
+//  arr a[0] : 11
+//  arr a[1] : 0
+//  arr a[2] : 15
+//  arr a[3] : 6
+//  arr a[4] : 3
+//  arr a[5] : 8
+//  arr a[6] : 3
+//  arr a[7] : 8
+//  arr a[8] : 7
+//  arr a[9] : 14
+//  arr a[10] : 9
+//  arr a[11] : 0
+//  arr a[12] : 7
+//  arr a[13] : 10
+//  arr a[14] : 7
+
 // cosntraint for two  random variables such that one var 
 // should not match other and total number of bits toggled in one var should be 5 with respect to the other
 
 class pattern_2;
     rand bit [7:0] a,b;
+
+    constraint match {a != b;}
+    constraint ones {$countones(a) == 5;}
+
 endclass
+
+module top;
+    pattern_2 p1;
+
+    initial begin
+        p1=new();
+        repeat(4) begin
+            p1.randomize();
+            $display("A : %0d \n",p1.a);
+            $display("B : %0d \n",p1.b);
+        end
+
+    end
+    endmodule

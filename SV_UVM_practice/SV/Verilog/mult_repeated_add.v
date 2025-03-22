d1f56f30c63bebdcc3a7bf003d0a9dd7fc0e4fe4
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-03-21 17:45:01 -0700

    updated SV_UVM_practice/SV/Verilog/mult_repeated_add.v SV_UVM_practice/SV/Verilog/tb_mult_repeated_add.v

--- SV_UVM_practice/SV/Verilog/mult_repeated_add.v
+++ SV_UVM_practice/SV/Verilog/mult_repeated_add.v
@@ -110,3 +110,37 @@ module controller (
     end
 
 endmodule
+
+// `include "mult_repeated_add.v"
+// `default_nettype none
+
+module tb_mult;
+reg [15:0] data_in;
+reg clk, start;
+reg rst_n;
+wire done;
+
+MUL_datapath DP (eqz,LdA,LdB,LdP,clrP,decB,data_in,clk);
+controller CON (LdA,LdB,LdP,clrP,decB,done,clk,eqz,start);
+
+localparam CLK_PERIOD = 10;
+always #(CLK_PERIOD/2) clk=~clk;
+
+initial begin
+    clk = 1'b0;
+    #3 start = 1'b1;
+    #500 $finish;
+end
+initial begin
+    #17 data_in = 17;
+    #10 data_in = 5;
+end
+
+initial begin
+    $monitor ($time, "%d %b", DP.y,done);
+    $dumpfile ("mul.vcd");
+    $dumpvars (0,tb_mult);
+end
+
+endmodule
+// `default_nettype wire

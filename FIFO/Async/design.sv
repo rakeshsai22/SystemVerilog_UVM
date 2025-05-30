3b98307b694cab21a3cffd6a341e4760fcc856ed
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-05-29 17:45:00 -0700

    updated FIFO/Async/design.sv

--- FIFO/Async/design.sv
+++ FIFO/Async/design.sv
@@ -8,3 +8,38 @@
 
 //  EMty condition: rd_ptr == wr_ptr   
 //  Full condition: wr_ptr[FIFO_DEPTH],]
+
+
+// Code your design here
+
+module examplfsm(input logic clk,
+                 input logic rst,
+                 input logic X,
+                 output logic Y);
+  
+  typedef enum logic [2:0]{A,B,C,D,E} State;
+  State currentState, nextState;
+  
+  always_ff @(posedge clk)
+    if(rst) currentState <= A;
+  else currentState<= nextState;
+  
+  always_comb
+    case (currentState)
+      A:{nextState,Y} = X?{C,1'b0}:{B,1'b1};
+//       A:if(X) nextState = C;
+//       	else nextState = B;
+      B:if(X) nextState = D;
+      	else nextState = B;
+      C:if(X) nextState = C;
+      	else nextState = E;
+      D:if(X) nextState = C;
+      	else nextState = E;
+      E:if(X) nextState = D;
+      	else nextState = B;
+      default: nextState = A;
+    endcase
+//   assign Y = (currentState == D | currentState == E);
+        
+endmodule
+        

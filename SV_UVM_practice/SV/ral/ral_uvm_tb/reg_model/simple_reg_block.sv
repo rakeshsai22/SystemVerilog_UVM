3d4e6de76ba8aa7d516cdd032df640fa699dc9d5
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-06-12 11:44:00 -0700

    updated files 06_12

--- SV_UVM_practice/SV/ral/ral_uvm_tb/reg_model/simple_reg_block.sv
+++ SV_UVM_practice/SV/ral/ral_uvm_tb/reg_model/simple_reg_block.sv
@@ -9,10 +9,11 @@ class simple_reg_block extends uvm_reg_block;
     endfunction
 
     virtual function void build();
-        default_map = create_map("default_map",0,1,UVM_LITTLE_ENDIAN); //little endian?
+        default_map = create_map("default_map",0,1,UVM_LITTLE_ENDIAN); 
         cfg_reg = config_reg::type_id::create("cfg_reg");
         cfg_reg.build();
-        cfg_read.configure(this,null,"cfg_reg");
+        cfg_reg.configure(this,null,"cfg_reg");
+        cfg_reg.set_hdl_path("config_reg.sv")
         cfg.lock_model();
         default_map.add_reg(cfg_reg, 'h0, "RW");
         endfunction

68ba70c01b570d120dac005a2d7eb063dbdc9bf1
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-06-11 18:04:58 -0700

    updated SV_UVM_practice/SV/ral/ral_uvm_tb/tb/env/ral_env.sv SV_UVM_practice/SV/ral/ral_uvm_tb/tb/tb_top.sv

--- SV_UVM_practice/SV/ral/ral_uvm_tb/tb/env/ral_env.sv
+++ SV_UVM_practice/SV/ral/ral_uvm_tb/tb/env/ral_env.sv
@@ -1,6 +1,9 @@
 class my_env extends uvm_env;
 
     simple_reg_block reg_blk;
+    apb_agent agent;
+    my_reg_adapter adapter;
+
     `uvm_component_utils(my_env)
 
     function new(string name,uvm_component parent);
@@ -9,16 +12,27 @@ class my_env extends uvm_env;
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
+        // apb agent creation
+        agent= apb_agent::type_id::create("agent",this);
+
+        // ral block creation
         reg_blk = simple_reg_block::type_id::create("reg_blk", this);
         reg_blk.build();
         reg_blk.lock_model();
         reg_blk.set_hdl_path_root("top.dut"); //rtl path for backdoor
 
+        // adapter creation
+        adapter= my_reg_adapter::type_id::create("adapter");
+
+        // sharing reg blk through configdb for sequences
+        uvm_config_db #(simple_reg_block)::set(this,"*","reg_blk",reg_blk);
+
     endfunction
 
     function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
-        //adapter connection
+        //adapter connection :: reg model to sequencer
+        reg_blk.default_map.set_sequencer(agent.sqr,adapter);
     endfunction
 
 endclass //my_env extends superClass
---
+++ SV_UVM_practice/SV/ral/ral_uvm_tb/tb/tb_top.sv
@@ -0,0 +1,36 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+module tb_top;
+
+    logic clk;
+    logic rst_n;
+
+    // dut interface
+    apb_if apb_if_inst(clk);
+
+    simple_config_reg dut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .wr_en(apb_if_inst.psel & apb_if_inst.penable & apb_if_inst.pwrite),
+        .wr_data(apb_if_inst.pwdata),
+        mode_o(),
+        enable_o()
+    );
+
+    initial begin
+        clk=0;
+        rst_n=0;
+        always #5 clk = ~clk;
+        #20;
+        rst_n=1;
+
+    end
+
+    initial begin
+        uvm_config_db #(virtual apb_if)::set(null,"*",apb_if_inst);
+        run_test("my_test");
+    end
+
+
+endmodule

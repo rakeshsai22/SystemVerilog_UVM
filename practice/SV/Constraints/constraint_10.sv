7b1f804872f86e8efd02920877502319f4588c38
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-08-03 17:45:01 -0700

    updated practice/SV/Constraints/constraint_10.sv

--- practice/SV/Constraints/constraint_10.sv
+++ practice/SV/Constraints/constraint_10.sv
@@ -17,30 +17,85 @@
 // c3: 10% low + 10% high + 80% mid
 // c4: 33% low,33% mid,33% high
 
-module pixel_arr_top();
+
+class Pixel;
     
-    parameter ROWS = 16;
-    parameter COLS = 32;
-    parameter TOTAL_PIXELS = ROWS * COLS;
+    rand logic [9:0] value;
 
-    typedef logic [9:0] pixel_t;
-    pixel_t pixel_arr [ROWS][COLS];
+    // C1: 10% high values only (>= 922)
 
-    task automatic gen_pixels_c1(output pixel_t pixels[ROWS][COLS]);
-        foreach (pixels[i][j]) begin
-            pixels[i][j] = $urandom_range(922,1023);
-        end
-        endtask
+    constraint c_1 {
+        value inside {[922:1023]};
+    }
 
-    task automatic gen_pixels_c2(output pixel_t pixels[ROWS][COLS]);
-        foreach (pixels[i][j]) begin
-            pixels[i][j] = $urandom_range(819,1023);
-        end
-    endtask
+    // c2: 20% high values only(>= 819)
+    constraint c_2 {
+        value inside {[819:1023]};
+    }
 
-    task automatic gen_pixels_c3(output pixel_t pixels[ROWS][COLS]);
+    constraint c_3 {
+        value inside {[0:1023]};
+    }
+
+    function void display();
+        $display("Pixel = %0d", value);
+    endfunction
+endclass
+
+module pixel_tb();
+    
+    parameter int ROWS  = 16;
+    parameter int COLS  = 32;
+    parameter int TOTAL = ROWS * COLS;
+
+    Pixel pixels[TOTAL];
+
+    initial begin
         foreach (pixels[i]) begin
-            
+            pixels[i] = new();
         end
-    endtask
+        $display("C1:100% high(>= 922)");
+        foreach (pixels[i]) begin
+            assert(pixels[i].randomize() with { value inside {[922:1023]}; });
+        end
+
+        $display("C2: 100% Low,80% Mid, 10% High");
+        int low = TOTAL * 0.10;
+        int mid = TOTAL * 0.80;
+        int high = TOTAL - low - mid;
+
+        foreach (pixels[i]) begin
+            if(i<low) begin
+                assert(pixels[i].randomize() with { value inside {[0:341]}; });
+            end
+            else if (i< (low+mid)) begin
+                assert(pixels[i].randomize() with { value inside {[342:682]}; });
+            end
+            else begin
+                assert(pixels[i].randomize() with { value inside {[683:1023]}; });
+            end
+
+        end
+
+        // C4: Equal low/mid/high
+        $display("C4")
+        int each = TOTAL/3;
+        foreach (pixels[i]) begin
+            if(i<each) begin
+                assert(pixels[i].randomize() with { value inside {[0:341]}; });
+            end
+            else if(i< 2* each) begin
+                assert(pixels[i].randomize() with { value inside {[342:682]}; });
+            end
+            else begin
+                assert(pixels[i].randomize() with { value inside {[683:1023]}; });
+            end
+        end
+
+        foreach (pixels[i]) begin
+            if(i%50 == 0) 
+                pixels[i].display();
+        end
+        $finish;
+    end
 endmodule

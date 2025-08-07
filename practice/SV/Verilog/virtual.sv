3b256ca0ab53c0f8c3ee98bdf002c4243f3f9640
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-08-06 22:09:06 -0700

    updated practice/SV/Verilog/virtual.sv

---
+++ practice/SV/Verilog/virtual.sv
@@ -0,0 +1,8 @@
+// Virtual methods
+
+// a virtual method or task from the base class can be overridden by a method of its child class having the same method name and arguments
+
+// ?? virtual class / abstract class 
+
+
+

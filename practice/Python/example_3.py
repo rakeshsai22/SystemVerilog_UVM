9ba5f753569ef33126703fcea2a68aa8a2e13fb8
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-08-15 21:32:23 -0700

    updated practice/Python/example_3.py

--- practice/Python/example_3.py
+++ practice/Python/example_3.py
@@ -1,3 +1,18 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+        # nums    : [ a sorted (asc) array of unique integers]
+        # target  : int to find 
+        # return  : true if exists
+        #         : -1 if not
+        # runtime : O(logn)
+        # space   : O(1)
+        # n       : size of input array
+        # l       : left end of the segement 
+        # r       : right end of the segement
+
+        mid = int(len(nums)/2)
+        if(target > mid):
+            
+
+
+

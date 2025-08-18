532db9d3d8e903688e4116d4f21d7495e9795bcf
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-08-18 09:03:02 -0700

    updated practice/Python/example_4.py

--- practice/Python/example_4.py
+++ practice/Python/example_4.py
@@ -8,15 +8,46 @@ class Solution:
         # else : 
         #   return false
 
-        m, n = len(matrix), len(matrix[0])
-        l, r = 0, m * n - 1
+        m = len(matrix)
+        n = len(matrix[0])
+        # l, r = 0, m * n - 1
 
+        # while l <= r:
+        #     mid = (l + r) // 2
+        #     i = mid // n
+        #     j = mid % n
+        #     val = matrix[i][j]
+
+        #     if val == target:
+        #         return True
+        #     elif val < target:
+        #         l = mid + 1
+        #     else:
+        #         r = mid - 1
+
+        # return False
+
+        # binary search to find the only possible row
+        top, bot = 0, m - 1
+        row = -1
+        while top <= bot:
+            mid = (top + bot) // 2
+            if matrix[mid][0] <= target <= matrix[mid][n - 1]:
+                row = mid
+                break
+            elif target < matrix[mid][0]:
+                bot = mid - 1
+            else:  # target > matrix[mid][n - 1]
+                top = mid + 1
+
+        if row == -1:
+            return False  # no row can contain target
+
+        # binary search within that row
+        l, r = 0, n - 1
         while l <= r:
             mid = (l + r) // 2
-            row = mid // n
-            col = mid % n
-            val = matrix[row][col]
-
+            val = matrix[row][mid]
             if val == target:
                 return True
             elif val < target:
@@ -25,7 +56,6 @@ class Solution:
                 r = mid - 1
 
         return False
-
     # check the target if its less than or greater than the first element
     # and proceed to the row which matches. and check if the last element 
     # is lesser than the target so we need not check the entire row.

9a708cfaac4d40fd693542074396b53368369c14
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-08-19 17:45:01 -0700

    updated practice/c/Dynamic_Memory_Allocation/Note_1b.c

--- practice/c/Dynamic_Memory_Allocation/Note_1b.c
+++ practice/c/Dynamic_Memory_Allocation/Note_1b.c
@@ -2,6 +2,8 @@
 
 // Deallocates the space previously allocated by malloc(),calloc(),realloc()
 
+// The allocator itself knows the size of the allocated block (it stored it internally when you called malloc), so the caller does not pass the size
+
 //  if a ptr is a null pointer, the function does nothing
 // The behavior is undefined , if the value of the ptr does not equal a valiue returned by malloc,calloc,realloc,
 // The behavior is undefined if the memory area referred to by ptr has already been deallocated by free(),realloc(),...
@@ -30,6 +32,11 @@ int main(void){
 
 // Deallocates the space previously allocated by malloc(),calloc(),or realloc() (but not aligned_alloc())
 
+// Same as free(), but you tell it the size of the allocation that you are freeing.
+// The allocator may use this hint to improve performance because it doesn’t need to look up the 
+// allocation size in its metadata. Only valid if size is exactly the size originally passed to 
+// malloc/calloc/realloc. Passing the wrong size leads to undefined behavior.
+
 // Parameters 
 //     ptr : pointer to the memory deallocate
 //     size: size of memory previously passed to an allocated function
@@ -51,6 +58,33 @@ typedef struct{
 
 PtrVector vector_create(size_t initial_capacity){
     PtrVector ret = {
-        
+        .capacity = initial_capacity,
+        .data = (void**) malloc(initial_capacity * sizeof(void*))
+    };
+    return ret;
+}
+
+void vector_delete(PtrVector* self){
+    free_sized(self->data,self->capacity * sizeof(void*));
+}
+
+void vector_push_back(PtrVector* self, void* value){
+    if(self->size == self->capacity){
+        self->capacity *= 2;
+        self->data = (void*) realloc(self->data, self->capacity * sizeof(void*));
     }
+    self->data[self-> size++] = value;
+}
+
+int main(){
+
+    int data = 42;
+    float pi = 3.14;
+    PtrVector v = vector_create(8);
+    vector_push_back(&v,&data);
+    vector_push_back(%v, $pi);
+    ptintf("data[0] = %i\n", *(int*)v.data[0]);
+    printf("data[1] = %f\n", *(float*)v.data[1]);
+    vector_delete(&v);
+    
 }

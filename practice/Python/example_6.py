052b89e9a27c03f26592ae04cdd65c1e270a78b4
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-08-26 19:14:38 -0700

    updated practice/Python/example_6.py

--- practice/Python/example_6.py
+++ practice/Python/example_6.py
@@ -1,3 +1,13 @@
+# list comprehensions:
+#  You are given three integers  and  representing the dimensions of a cuboid along with an integer . 
+# Print a list of all possible coordinates given by  on a 3D grid where the sum of  is not equal to . 
+# Here, . Please use list comprehensions rather than multiple loops, as a learning exercise.
+
+# Print an array of the elements that do not sum to n.
+
+
+
+
 if __name__ == '__main__':
     x = int(input())
     y = int(input())

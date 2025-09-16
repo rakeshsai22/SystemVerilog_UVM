69b63baf2875845604086cde61f8a980508d7aea
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-09-16 02:37:42 -0700

    updated practice/SV/Verilog/sv_dv_4.sv

--- practice/SV/Verilog/sv_dv_4.sv
+++ practice/SV/Verilog/sv_dv_4.sv
@@ -1,3 +1,19 @@
+// // Code your design here
+// Question:
+// Write a SystemVerilog module that implements a synchronous FIFO with the following requirements:
+
+// Parameters for depth and data width.
+
+// FIFO should support write_en, read_en, data_in, data_out.
+
+// Output a flag for empty and full.
+
+// Use always_ff for sequential logic and always_comb for combinational logic.
+
+// Follow-up:
+
+// How would you write a SystemVerilog assertion to check that the FIFO is never written when full is high?
+
 // Question:
 // Write a SystemVerilog module that implements a synchronous FIFO with the following requirements:
 // Parameters for depth and data width.
@@ -8,12 +24,12 @@
 // How would you write a SystemVerilog assertion to check that the FIFO is never written when full is high?
 
 module sync_fifo #(
-    parameter DATAWIDTH,
-    parameter DEPTH
+    parameter DATAWIDTH = 16,
+    parameter DEPTH =8
 ) (
     input logic clk,rst_n,
     input logic [DATAWIDTH-1:0] data_in,
-    input logic [DATAWIDTH-1:0] data_out,
+    output logic [DATAWIDTH-1:0] data_out,
     input logic write_en,
     input logic read_en,
     output logic full,
@@ -33,12 +49,17 @@ module sync_fifo #(
             sync_fifo[wptr] <= data_in;
             wptr <= wptr+1;
         end
+        // else $display()
         if (read_en && !empty) begin
             data_out <= sync_fifo[rptr];
             rptr<=rptr+1;
         end
+        // else $display()
         else begin
-            empty > full ? $display("FIFO is empty") : $display("FIFO is full");
+//           (empty > full) ? $display("FIFO is empty") : $display("FIFO is full");
+          $display( (read_en && empty) ? "FIFO is empty"
+                  : (write_en && full) ? "FIFO is full"
+                  : "No valid read/write this cycle" );
         end
     end
 

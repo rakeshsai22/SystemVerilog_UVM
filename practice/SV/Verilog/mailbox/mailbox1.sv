32ed2c145b0a504023f98598b0579e4cb53c46bd
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2025-12-31 10:52:47 -0700

    updated practice/SV/Verilog/mailbox/mailbox1.sv

--- practice/SV/Verilog/mailbox/mailbox1.sv
+++ practice/SV/Verilog/mailbox/mailbox1.sv
@@ -1,4 +1,14 @@
 // mailbox is a way to allow different processes to exchange data betwen each other
 //  created with a bounded or unbounded queue size
+// Mailbox behaves as a FIFO
+// where is it used?
+//      communication between different blocks in the tb
 // ## teo types of mailboxes 
-//      1. Generic - accepts any data type
+//      1. Generic       - accepts any data type (int,bit,str..) [default type]
+//      2. Parameterized - accepts only a particular data_type
+// 
+
+// Syntax:
+//      mailbox #(<type>)<mailbox_name>
+
+ 

b44edce878c3464946807c398338f561f703b005
rakeshsai22 <168497984+rakeshsai22@users.noreply.github.com>
2026-01-06 19:11:37 -0700

    updated practice/cocotb/mux/tb_mux_cpp.cpp

--- practice/cocotb/mux/tb_mux_cpp.cpp
+++ practice/cocotb/mux/tb_mux_cpp.cpp
@@ -1,5 +1,4 @@
-// #include "mux_2x1.h"
-// #include "verilated.h"
+// #include "Vmux2x1.h"
 #include <iostream>
 
 int main(int argc, char** argv) {
@@ -9,7 +8,7 @@ int main(int argc, char** argv) {
 
     dut.a = 0;
     dut.b = 0;
-    dut.sel = 0;
+    dut.sel = 0;a
     dut.eval();
     std::cout << "sel=0, out=" << (int)dut.out << std::endl;
 
@@ -20,10 +19,6 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-
-// - V e r i l a t i o n   R e p o r t: Verilator 5.038 2025-07-08 rev UNKNOWN.REV
-// - Verilator: Built from 0.000 MB sources in 0 modules, into 0.000 MB in 0 C++ files needing 0.000 MB
-// - Verilator: Walltime 0.001 s (elab=0.000, cvt=0.000, bld=0.000); cpu 0.001 s on 1 threads
 
 // - V e r i l a t i o n   R e p o r t: Verilator 5.038 2025-07-08 rev UNKNOWN.REV
 // - Verilator: Built from 0.025 MB sources in 2 modules, into 0.025 MB in 7 C++ files needing 0.000 MB
